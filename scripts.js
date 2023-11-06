40e404cf800e7fd9cad1016512e1e1999ebcd93e
Gheceff <91993800+Gheceff@users.noreply.github.com>
2023-11-06 23:17:41 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,87 @@
+
+// Exercitiul 1
+var a=5 ,b=25 ,c=50
+
+
+console.log("Adunare",a+b);
+console.log("Inmultire",a*(b*a))
+console.log("Scadere",b-c);
+
+
+
+// Exercitiul 2
+var a=5,b=10,c=15,d=20,e=25,f=30
+var sum = a+b+c+d+e+f
+console.log("Suma",sum)
+
+var produs = a * b * c * d * e * f;
+console.log("Produs", produs)
+var x = 105;
+var y = 6;
+var z = x / y;
+console.log("Media aritmetica",105/6)
+
+
+
+
+// Exercitiul 3
+console.log("Numar normal 7:", 7)
+
+
+// Exercitiul 4
+console.log("Numar maxim" , Math.max(1,4,-10,56,89));
+
+
+// Exercitiul 5
+console.log("Partea superioara" , Math.round(14.3))
+
+// Exercitiul 6
+console.log("Partea inferioara" , Math.floor(14.3))
+
+// Exercitiul 7
+console.log("Valoare Absoluta",Math.abs(0))
+// Exercitiul 8
+console.log("Numar random",Math.random(0,1))
+
+
+
+// Observatii
+
+
+/* In prima instanta , se initializeaza variabilele numerice - x si y , ca pe urma , sa se introduca o operatiune matematica in aflarea variabilei z.
+Tipul operatiunii matematice este adunarea.*/
+var x = 10;
+var y = 20;
+var z = x + y;
+
+
+/* In prima instanta , se initializeaza variabilele de tip string - x si y , ca pe urma , sa se introduca o operatiune matematica in aflarea variabilei z.
+Tipul operatiunii matematice este adunarea.*/
+var x = "10";
+var y = "20";
+var z = x + y;
+
+
+/* In prima faza , se initializeaza variabila de tip numeric - x si si pe urma , variabila de tip string - y . Dupa  se introduce o operatiune matematica in aflarea variabilei z.
+Tipul operatiunii matematice este adunarea. */
+var x = 10;
+var y = "20";
+var z = x + y
+
+/* In prima instanta , se initializeaza variabilele de tip string - x si y , ca pe urma , sa se introduca o operatiune matematica in aflarea variabilei z.
+Tipul operatiunii matematice este impartirea.*/
+var x = "100";
+var y = "10";
+var z = x / y;
+
+/* In prima instanta , se initializeaza variabilele de tip string - x si y , ca pe urma , sa se introduca o operatiune matematica in aflarea variabilei z.
+Tipul operatiunii matematice este inmultirea.*/
+var x = "100";
+var y = "10";
+var z = x * y;
+
+/* In prima instanta , se initializeaza variabilele de tip string - x si y , ca pe urma , sa se introduca o operatiune matematica in aflarea variabilei z.
+Tipul operatiunii matematice este adunarea.*/
+var x = "100";
+var y = "10";
+var z = x + y
